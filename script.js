973f6b31e8588a2bca9e45ad50b9333174887a87
karan <kskular05@gmail.com>
2023-08-03 23:09:45 +0530

    hover over the divs

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ let horizontalGridNum = 16;
 let verticalGridNum = 16;
 const container = document.querySelector(".container");
 
+function hover(div){
+    div.classList.toggle("hoveredOver");
+}
+
 function makeGrid(width, height, horizontalGridNum, verticalGridNum){
     for(let i = 0; i < horizontalGridNum * verticalGridNum; i++){
         let div = document.createElement("div");
         div.classList.add("box");
         div.classList.add("hoveredOver");
+        div.addEventListener("mouseenter", () => {div.classList.toggle("hoveredOver")
+                                                  div.style.zIndex = 1;});
+        div.addEventListener("mouseleave", () => {div.classList.toggle("hoveredOver")
+                                                  div.style.zIndex = 0;});
         div.classList.add("clicked");
         div.classList.toggle("hoveredOver");
         div.classList.toggle("clicked");
